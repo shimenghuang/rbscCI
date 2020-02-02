87b4d410ea09786e677550ee8f45ee6e38d97a88
Shimeng Huang <dora.huang.sunshine@gmail.com>
2020-02-02 17:07:14 -0500

    Added argument check.

--- R/bscCI.R
+++ R/bscCI.R
@@ -1,4 +1,6 @@
 #' Blyth-Still-Casella confidence interval
+#' 
+#' @details Comuputes the exact Blyth-Still-Casella binomial confidence interval. The initial CI is the Clopper-Pearson confidence interval.
 #'
 #' @param n_tot Total number of experiments
 #' @param n_suc Number of successes
@@ -6,6 +8,16 @@
 #' @details ...
 #'
 #' @export bscCI
-bscCI <- function(n_tot, n_suc, conf) {
-  .bscCI(n_tot, n_suc, conf)
+bscCI <- function(n_tot, n_suc, conf, digits = 2) {
+  
+  stopifnot(n_tot > 0 && n_suc > 0 && n_tot >= n_suc)
+  stopifnot(conf > 0 && conf < 1)
+  
+  bsc_ci <- .bscCI(n_tot, n_suc, conf)
+  if (!missing(digits)) {
+    round(bsc_ci, digits)
+  }
+  else {
+    return(bsc_ci)
+  }
 }
